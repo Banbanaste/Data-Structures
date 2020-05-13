17469b38722839216d985a331c5ecdea8695db08
Roberto Banbanaste <rbanbanaste@outlook.com>
2020-05-13 18:54:20 -0400

    part 1 complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,28 +9,60 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
+    def __str__(self):
+        return "(node value: " + str(self.value) + " node left: %s node right: %s) " % (self.left, self.right)
+
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if self.value == value:
+            return False
+        node = BSTNode(value)
+        if value < self.value:
+            if self.left is None:
+                self.left = node
+            self.left.insert(value)
+        if value > self.value:
+            if self.right is None:
+                self.right = node
+            self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if target is self.value:
+            return True
+
+        if target < self.value:
+            if not self.left:
+                return False
+            return self.left.contains(target)
+
+        else:
+            if not self.right:
+                return False
+            return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if not self.right:
+            return self.value
+        return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
+        return fn(self.value)
 
     # Part 2 -----------------------
 
@@ -59,3 +91,21 @@ class BSTNode:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+
+
+""" 
+
+bst = BSTNode(5)
+bst.insert(2)
+bst.insert(3)
+bst.insert(7)
+bst.insert(6)
+
+print(bst)
+
+"""
+
+bst2 = BSTNode(1)
+bst2.insert(1)
+
+print(bst2.value)
